d66a5bc6307b851f88a4ef5fe42905fba243caa0
LukasDrsman <lukaskodr@gmail.com>
2022-05-16 17:33:30 +0200

    Fix comment

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define PATH_LEN 256  // depends on SENSOR_PREFIX and MOTOR+PREFIX
+#define PATH_LEN 256  // depends on SENSOR_PREFIX and MOTOR_PREFIX
 #define SENSOR_PREFIX "/sys/class/lego-sensor/"
 #define MOTOR_PREFIX  "/sys/class/tacho-motor/"
 
