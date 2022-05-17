2e0f8f97be4dd831217c075ee753d4458272db11
LukasDrsman <lukaskodr@gmail.com>
2022-05-17 10:37:12 +0200

    Add testing environment

---
+++ testenv/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "../src/motor.h"
+#include "../src/sensor.h"
+
+int main(int argc, char *argv[])
+{
+    motor m;
+    m = motorNew('A');
+
+    if (!m.exists) return 1;
+
+    motorReset(m);
+    motorSetSpeed(m, 200);
+    motorSetTarget(m, 360);
+    motorSetStopAction(m, "hold");
+    motorCommand(m, "run-to-rel-pos");
+
+    while (motorState(m) & RUNNING);
+
+    return 0;
+}
