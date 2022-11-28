60864c9e85b3c6534e3111c61c3f3810a046b840
Lukáš Dršman <lukaskodr@gmail.com>
2022-11-28 20:18:46 +0100

    Implement fetch

--- src/controller.c
+++ src/controller.c
@@ -1,15 +1,60 @@
 #include "controller.h"
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
-int controllerConnect(char *addr, int port)
+
+controller controllerConnect(char *addr, int port)
 {
-    int ctl_fd, client_fd;
-    struct sockaddr_in serv_addr;
+    controller c;
+    struct sockaddr_in address;
 
-    if ((ctl_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) return -1;
+    c.status = CSTAT_OK;
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = (uint32_t) port;
+    if ((c.socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        c.status |= CSTAT_CREATION_ERROR;
+        return c;
+    }
 
-    inet_pton(AF_INET, addr, &serv_addr.sin_addr);
+    address.sin_family = AF_INET;
+    address.sin_port = (uint32_t) port;
 
+    if (inet_pton(AF_INET, addr, &address.sin_addr) <= 0)
+    {
+        c.status |= CSTAT_INVALID_ADDRESS;
+        return c;
+    }
+
+    if ((c.client = connect(c.socket, (struct sockaddr *) &address, sizeof(address))) < 0)
+    {
+        c.status |= CSTAT_CONNECTION_ERROR;
+        return c;
+    }
+
+    return c;
+}
+
+int controllerGet(controller c, resource r)
+{
+    char fetch = (char) r;
+    send(c.socket, &fetch, 1);
+
+    if (r >= LeverLX)
+    {
+        char buffer[2];
+        read(c.socket, buffer, 2);
+
+        int value;
+        value  = (int) buffer[1] + 127;
+        value *= (!buffer[0] - buffer[0]);
+
+        return value;
+    }
+
+    char value;
+    read(c.socket, &value, 1);
+
+    return (int) value;
 }
--- src/controller.h
+++ src/controller.h
@@ -1,9 +1,30 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
-#define DEFAULT_CONTROLLER_PORT 8989
-#define DEFAULT_CONTROLLER_ADDR NULL
+#define CDEF_PORT 8989
+#define CDEF_ADDRESS NULL
 
-int controllerConnect(char *addr, int port);
+#define CSTAT_OK                0b000
+#define CSTAT_CREATION_ERROR    0b001
+#define CSTAT_INVALID_ADDRESS   0b010
+#define CSTAT_CONNECTION_ERROR  0b100
+
+typedef struct Controller
+{
+    int socket, client, status;
+}
+controller;
+
+typedef enum Resource
+{
+    A, B, X, Y,
+    PadUP, PadDOWN, PadLEFT, PadRIGHT,
+    ButtonL1, ButtonL2, ButtonR1, ButtonR2,
+    LeverLX, LeverLY, LeverRX, LeverRY
+}
+resource;
+
+controller controllerConnect(char *addr, int port);
+int controllerGet(controller c, resource r);
 
 #endif
